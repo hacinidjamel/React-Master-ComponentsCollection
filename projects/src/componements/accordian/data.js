ab775cf4b4735ae69c48b1612991bb2540b68e51
djaam <djamel@gmail.com>
2024-06-23 19:29:15 +0100

    i add new user to my data

--- projects/src/componements/accordian/data.js
+++ projects/src/componements/accordian/data.js
@@ -1,21 +1,27 @@
 const data = [
     {
         age: 24,
-        id: 0,
+        id: 1,
         name: 'djamel',
         position: "softwar engineer",
         summary: "i trust my self i know i can handle it "
     },
     {
-        age: 25,
-        id: 1,
+        age: 23,
+        id: 2,
         name: 'mossab',
         position: "IT Support",
         summary: "my hobbis get me to to this position"
+    }, {
+        age: 24,
+        id: 3,
+        name: 'youcef',
+        position: "Data Analyst",
+        summary: "as a junior developper i can solve any basic problem"
     },
     {
-        age: 26,
-        id: 2,
+        age: 25,
+        id: 4,
         name: 'abdennour',
         position: "Graphic designer",
         summary: "i am just good at my domaine"
